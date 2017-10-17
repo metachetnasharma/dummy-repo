5fd3557d26e2a9049a419a5b81ee9b8e0aebf12e
Shobhit Dixit <shobhit.dixit@metacube.com>
2017-10-17 15:44:00 +0530

    changes in dev

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module DummyApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     EmailVerifier.config do |config|
-      config.verifier_email = "realname@realdomain.com"
+      config.verifier_email = "realname@realdomain1.com"
     end
   end
 end
